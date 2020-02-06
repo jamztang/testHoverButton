5688ddd6e7a753617f149a2f0c45b5686eb89ce1
James Tang <james@goodnotesapp.com>
2020-02-06 10:04:05 +0800

    Initial Commit

---
+++ testHoverButton/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  testHoverButton
+//
+//  Created by James Tang on 6/2/2020.
+//  Copyright © 2020 James Tang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ testHoverButtonTests/testHoverButtonTests.swift
@@ -0,0 +1,34 @@
+//
+//  testHoverButtonTests.swift
+//  testHoverButtonTests
+//
+//  Created by James Tang on 6/2/2020.
+//  Copyright © 2020 James Tang. All rights reserved.
+//
+
+import XCTest
+@testable import testHoverButton
+
+class testHoverButtonTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
